8531c79e42e3bcf77d44d61a49738a215a9970d2
aphasia0 <tony.dream@gmail.com>
2021-02-08 20:21:15 +0100

    Build

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {InquinamentoComponent} from './inquinamento/inquinamento.component';
 
+
 const routes: Routes = [
   { path: 'inquinamento-milano', component: InquinamentoComponent },
   { path: '',     loadChildren: () => import('./home/home.module').then((m) => m.HomeModule), },
